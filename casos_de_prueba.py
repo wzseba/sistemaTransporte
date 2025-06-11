412fa7636a357bcda050b93dc260f7b01f26c086
unknown <camilaariadnavera@gmail.com>
2025-06-11 19:55:02 -0300

    error en el constructor de la clase

--- casos_de_prueba.py
+++ casos_de_prueba.py
@@ -2,77 +2,78 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from reversion import Circuito_Ebike
 
-#-----------------------------------------------------------#
+# -----------------------------------------------------------#
 #                          Grafo 1                          #
-#-----------------------------------------------------------#
+# -----------------------------------------------------------#
 
-edificios_importantes = ["Casa Rosada","Catedral","Cabildo","Congreso","Teatro Colón","Edificio Kavanagh"]
+edificios_importantes = ["Casa Rosada", "Catedral",
+                         "Cabildo", "Congreso", "Teatro Colón", "Edificio Kavanagh"]
 
-lugares_secundarios = ["Casa1","Casa2","Casa3","Plaza de Mayo","Av de Mayo","Plaza San Martín"]
-
-                
-rutas = [("Casa Rosada","Cabildo",3,True),
-           ("Casa Rosada","Casa1",1,True),
-           ("Catedral","Casa2",1,True),
-           ("Catedral","Plaza de Mayo",1,True),
-           ("Catedral","Cabildo",3,True),
-           ("Plaza de Mayo","Cabildo",1,True),
-           ("Congreso","Av de Mayo",9,True),
-           ("Av de Mayo","Teatro Colón",4,True),
-           ("Edificio Kavanagh","Congreso",2,True),
-           ("Edificio Kavanagh","Plaza San Martín",4,True),
-           ("Plaza de Mayo","Teatro Colón",5,True),
-           ("Teatro Colón","Edificio Kavanagh",2,True),
-           ("Av de Mayo","Congreso",6,True),
-           ("Casa3","Plaza de Mayo",7,True)]
-
-circuito1 = Circuito_Ebike(edificios_importantes,lugares_secundarios,rutas)
+lugares_secundarios = ["Casa1", "Casa2", "Casa3",
+                       "Plaza de Mayo", "Av de Mayo", "Plaza San Martín"]
 
 
+rutas = [("Casa Rosada", "Cabildo", 3, True),
+         ("Casa Rosada", "Casa1", 1, True),
+         ("Catedral", "Casa2", 1, True),
+         ("Catedral", "Plaza de Mayo", 1, True),
+         ("Catedral", "Cabildo", 3, True),
+         ("Plaza de Mayo", "Cabildo", 1, True),
+         ("Congreso", "Av de Mayo", 9, True),
+         ("Av de Mayo", "Teatro Colón", 4, True),
+         ("Edificio Kavanagh", "Congreso", 2, True),
+         ("Edificio Kavanagh", "Plaza San Martín", 4, True),
+         ("Plaza de Mayo", "Teatro Colón", 5, True),
+         ("Teatro Colón", "Edificio Kavanagh", 2, True),
+         ("Casa3", "Plaza de Mayo", 7, True)]
 
-#caso_ruta_segura_para _e-bikes
-#Objetivo: Encontrar la ruta mas corta entre el Planetario y la Facultad de Derecho, usando solo aristas marcadas como segura.
+circuito1 = Circuito_Ebike(edificios_importantes, lugares_secundarios, rutas)
+
+
+# caso_ruta_segura_para _e-bikes
+# Objetivo: Encontrar la ruta mas corta entre el Planetario y la Facultad de Derecho, usando solo aristas marcadas como segura.
 
 """
-print(circuito1.caminoMinimoAlumno("Planetario Galileo Galilei", "Facultad de Derecho"))
+Resultado esperado: 🏢Edificio Kavanagh⇒Teatro Colón⇒Plaza de Mayo⇒🏫​Cabildo⇒🏛️Casa Rosada⇒🏡​Casa 1
 """
 
-#caso_peso_de_aristas_negativo
-#Objetivo: Detectar un error al usar valores negativos en el peso de las aristas.
+"""print(circuito1.caminoMinimoAlumno("Edificio Kavanagh", "Casa1"))"""
+
+
+# caso_peso_de_aristas_negativo
+# Objetivo: Detectar un error al usar valores negativos en el peso de las aristas.
 
 """
 circuito1.agregar_ruta("Casa1", "Congreso", -10, True)
 """
 
-#caso_nodos_inexistentes
-#Objetivo: Detectar un error al agregar aristas con nodos que no existen.
+# caso_nodos_inexistentes
+# Objetivo: Detectar un error al agregar aristas con nodos que no existen.
 
 """
 circuito1.agregar_arista("Mar del Plata", "Casa Rosada", 5, False)
 """
 
 
-
-
-#-----------------------------------------------------------#
-#                          Grafo 2                          #   
-#-----------------------------------------------------------#           
+# -----------------------------------------------------------#
+#                          Grafo 2                          #
+# -----------------------------------------------------------#
 edificios_importantes = [
-    "Obelisco", 
-    "Palacio Barolo", 
-    "Planetario Galileo Galilei", 
-    "Biblioteca Nacional", 
-    "Facultad de Derecho", 
+    "Obelisco",
+    "Palacio Barolo",
+    "Planetario Galileo Galilei",
+    "Biblioteca Nacional",
+    "Facultad de Derecho",
     "Teatro San Martín",
     "Museo Nacional de Bellas Artes",
     "Centro Cultural Kirchner"
 ]
 
 lugares_secundarios = [
-    "Calle Corrientes", 
-    "Avenida Santa Fe", 
-    "Plaza Italia", 
-    "Plaza Lavalle", 
+    "Calle Corrientes",
+    "Avenida Santa Fe",
+    "Plaza Italia",
+    "Plaza Lavalle",
     "Avenida Figueroa Alcorta",
     "Puerto Madero",
     "Parque Centenario"
@@ -99,14 +100,11 @@ rutas = [
 ]
 
 
-
-circuito2 = Circuito_Ebike(edificios_importantes,lugares_secundarios,rutas)
-
+circuito2 = Circuito_Ebike(edificios_importantes, lugares_secundarios, rutas)
 
 
-
-#caso_agregar_nuevo_lugar
-#Objetivo: Agregar un nuevo edificio a la lista de edificios principales y asegurarse de que siempre tenga una ruta
+# caso_agregar_nuevo_lugar
+# Objetivo: Agregar un nuevo edificio a la lista de edificios principales y asegurarse de que siempre tenga una ruta
 
 """
 circuito2.visualizar_circuito()
@@ -114,21 +112,16 @@ circuito2.agregar_ruta_y_edificio_principal("Teatro San Martín", "Facultad de C
 circuito2.visualizar_circuito()
 """
 
-#caso_calcular_ruta_camion
-#Objetivo: Calcular el camino mas corto que pase por todos los edificios principales
+# caso_calcular_ruta_camion
+# Objetivo: Calcular el camino mas corto que pase por todos los edificios principales
 """
 print(circuito2.calcularRutaCamion())
 """
 
-#caso_eliminar_lugar
-#Objetivo: Verificar que el lugar se elimine correctamente
+# caso_eliminar_lugar
+# Objetivo: Verificar que el lugar se elimine correctamente
 """"
 circuito2.visualizar_circuito()
 circuito2.eliminar_lugar("Teatro San Martín")
 circuito2.visualizar_circuito()
 """
-
-
-
-
-
--- reversion.py
+++ reversion.py
@@ -10,76 +10,82 @@ class Circuito_Ebike:
         self.grafo.add_nodes_from(edificios_principales)
         self.grafo.add_nodes_from(lugares_secundarios)
         # self.grafo.add_weighted_edges_from(Aristas)
-        
-        for origen, destino, distancia, segura in Rutas:
-            #Evita que se agregen nodos que no estan en ninguna de las listas
-            if(not(origen in self.edificios_principales or origen in self.edificios_principales)):
-                raise ValueError("El origen no es uno de los lugares previstos")
-            elif(not(destino in self.edificios_principales or destino in self.lugares_secundarios)):
-                raise ValueError("El destino no es uno de los lugares previstos")
-            
-            if(distancia < 0):
-                raise ValueError("La distancia no puede ser negativa")
-            
-            self.grafo.add_edge(origen, destino, weight=distancia, segura_para_ebike=segura)
 
+        for origen, destino, distancia, segura in Rutas:
+            # Evita que se agregen nodos que no estan en ninguna de las listas
+            if (not (origen in self.edificios_principales or origen in self.lugares_secundarios)):
+                raise ValueError(
+                    "El origen no es uno de los lugares previstos")
+            elif (not (destino in self.edificios_principales or destino in self.lugares_secundarios)):
+                raise ValueError(
+                    "El destino no es uno de los lugares previstos")
+
+            if (distancia < 0):
+                raise ValueError("La distancia no puede ser negativa")
+
+            self.grafo.add_edge(
+                origen, destino, weight=distancia, segura_para_ebike=segura)
 
     def agregar_ruta_y_edificio_principal(self, edificio_existente, edificio_nuevo, distancia, segura):
-        
+
         if (distancia < 0):
             raise ValueError("El distancia de la arista no puede ser negativo")
         if (edificio_nuevo in self.edificios_principales):
             raise ValueError("El nodo ya existe")
         elif (edificio_existente not in self.edificios_principales):
             raise ValueError("El nodo no existe")
-        
+
         self.grafo.add_node(edificio_nuevo)
         self.edificios_principales.append(edificio_nuevo)
-        
-         
-        self.grafo.add_edge(edificio_existente, edificio_nuevo, weight=distancia, segura_para_ebike=segura)
-    
+
+        self.grafo.add_edge(edificio_existente, edificio_nuevo,
+                            weight=distancia, segura_para_ebike=segura)
+
     def agregar_ruta_y_lugar_secundario(self, punto_existente, punto_nuevo, distancia, segura):
-        
+
         if (distancia < 0):
             raise ValueError("La distancia no puede ser negativa")
         if (punto_nuevo in self.edificios_principales):
             raise ValueError("El nodo ya existe")
         elif (punto_existente not in self.edificios_principales):
             raise ValueError("El nodo no existe")
-        
+
         self.grafo.add_node(punto_nuevo)
-        self.lugares_secundarios.append(punto_nuevo)  
-        
-         
-        self.grafo.add_edge(punto_existente, punto_nuevo, weight=distancia, segura_para_ebike=segura)
-        
+        self.lugares_secundarios.append(punto_nuevo)
+
+        self.grafo.add_edge(punto_existente, punto_nuevo,
+                            weight=distancia, segura_para_ebike=segura)
+
     def agregar_ruta(self, origen, destino, distancia, segura):
-        
+
         if (distancia < 0):
             raise ValueError("El distancia de la arista no puede ser negativo")
-        if (not(origen in self.edificios_principales or origen in self.lugares_secundarios)):
+        if (not (origen in self.edificios_principales or origen in self.lugares_secundarios)):
             raise ValueError("El origen no es uno de los nodos previstos")
-        elif (not(destino in self.edificios_principales or destino in self.lugares_secundarios)):        
+        elif (not (destino in self.edificios_principales or destino in self.lugares_secundarios)):
             raise ValueError("El destino no es uno de los nodos previstos")
-        
-        self.grafo.add_edge(origen, destino, weight=distancia, segura_para_ebike=segura)
-        
+
+        self.grafo.add_edge(origen, destino, weight=distancia,
+                            segura_para_ebike=segura)
+
     def caminoMinimoAlumno(self, origen, destino):
        # Función callback para Dijkstra
         def ebike_seguro(i, f, data):
             # Si la arista es segura para ebikes, devuelve su distancia.
             # De lo contrario, devuelve None para que la ignore.
             if data.get("segura_para_ebike", False):
-                return data.get("weight", 1) # Asume 1 si no hay 'weight' explícito
+                # Asume 1 si no hay 'weight' explícito
+                return data.get("weight", 1)
             else:
-                return None # Ignora esta arista
-        
+                return None  # Ignora esta arista
+
         # Intenta encontrar el camino más corto entre origen y destino
         try:
-            camino_minimo = nx.dijkstra_path(self.grafo, origen, destino, weight=ebike_seguro)
-            costo = nx.dijkstra_path_length(self.grafo, origen, destino, weight=ebike_seguro)
-            return camino_minimo, costo
+            camino_minimo = nx.dijkstra_path(
+                self.grafo, origen, destino, weight=ebike_seguro)
+            costo = nx.dijkstra_path_length(
+                self.grafo, origen, destino, weight=ebike_seguro)
+            return camino_minimo, "Costo total del camino: ", costo
         except nx.NetworkXNoPath:
             return None, "No hay camino seguro para ebikes entre los nodos especificados."
 
@@ -133,25 +139,23 @@ class Circuito_Ebike:
             self.edificios_principales.remove(nombre)
         elif nombre in self.lugares_secundarios:
             self.lugares_secundarios.remove(nombre)
-    
 
     def visualizar_circuito(self):
         layout = nx.spring_layout(self.grafo)  # diseño del grafo
-        labels = nx.get_edge_attributes(self.grafo, 'weight')  # obtiene los distancias
+        labels = nx.get_edge_attributes(
+            self.grafo, 'weight')  # obtiene los distancias
 
     # Dibuja el grafo
         nx.draw(self.grafo, pos=nx.circular_layout(self.grafo), with_labels=True,
-            node_color='lightblue', edge_color='gray',
-            node_size=1000, font_size=7, font_color='black', font_weight="bold")
+                node_color='lightblue', edge_color='gray',
+                node_size=1000, font_size=7, font_color='black', font_weight="bold")
 
     # Dibuja los distancias más cercanos a las aristas
         nx.draw_networkx_edge_labels(self.grafo,
-                                 pos=nx.circular_layout(self.grafo),
-                                 edge_labels=labels,
-                                 label_pos=0.4,  # ajusta la posición a lo largo de la arista
-                                 font_size=10,
-                                 font_color='blue')  # opcional: cambiar color para mejor visibilidad
+                                     pos=nx.circular_layout(self.grafo),
+                                     edge_labels=labels,
+                                     label_pos=0.4,  # ajusta la posición a lo largo de la arista
+                                     font_size=10,
+                                     font_color='blue')  # opcional: cambiar color para mejor visibilidad
 
         plt.show()
-        
-        
